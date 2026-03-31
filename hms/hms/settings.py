ce7bd5c541b117d203bb52477781de159edfc388
BhagyodayaBamgonda2 <gaminglegenda45@gmail.com>
2026-03-31 12:59:27 +0530

    requirements Update

--- hms/hms/settings.py
+++ hms/hms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-3h*la)3a=#29j&0390n#4h34xd1(y^k2d5rvnz^t*a(y&x*z20
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
